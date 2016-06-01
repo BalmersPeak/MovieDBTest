fb76bcbe4e427aa1bfb5e313902bb89dd1c28f10
Nick <Nick>
2016-05-31 22:30:44 -0400

    TmdbApp Tests and UMLDiagram

---
+++ tests/moviepack/TmdbAppTest.java
@@ -0,0 +1,13 @@
+package moviepack;
+
+import org.junit.Test;
+
+public class TmdbAppTest {
+
+    @Test
+    public void testsMain() {
+        //places holder for GUI test
+        TmdbApp tmdbApp = new TmdbApp();
+    }
+
+}
